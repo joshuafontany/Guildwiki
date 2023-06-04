40aefc0d4215888a6056e3b97c787540dd83af17
Joshua Fontany <joshua.fontany@gmail.com>
2023-06-04 04:48:59 -0700

    ODD template setup

---
+++ guildwiki-app/src/lib/app-info.ts
@@ -0,0 +1,6 @@
+export const appName = 'GuildWiki'
+export const appDescription = 'A Realtime Multiplayer Wiki Platform'
+export const appURL = 'https://guildwiki.app'
+export const appImageURL = `${appURL}/preview.png`
+export const ipfsGatewayUrl = 'runfission.com'
+export const oddNamespace = { creator: 'Fission', name: 'OAT' }
